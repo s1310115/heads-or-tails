6ffd79ed93039612ed43726fb0d051eb6e25fa12
Ryo8517 <r20040517@icloud.com>
2024-07-03 22:10:02 +0900

    HoT: initial revision

---
+++ HeadsOrTails.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+
+public class HeadsOrTails {
+    public static void main(String[] args) {
+        Random random = new Random();
+        
+        System.out.println("Tossing a coin...");
+        String[] results = new String[3];
+        
+        for (int i = 0; i < 3; i++) {
+            results[i] = random.nextBoolean() ? "Heads" : "Tails";
+            System.out.println("Round " + (i + 1) + ": " + results[i]);
+        }
+        
+        int headsCount = 0;
+        int tailsCount = 0;
+        for (String result : results) {
+            if (result.equals("Heads")) {
+                headsCount++;
+            } else {
+                tailsCount++;
+            }
+        }
+        
+        System.out.println("Heads: " + headsCount + ", Tails: " + tailsCount);
+    }
+}
