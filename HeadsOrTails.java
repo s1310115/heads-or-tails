86fd1614579db7e2e6429b83d4c7e7de2a1944b4
Ryo8517 <r20040517@icloud.com>
2024-07-03 23:44:11 +0900

    HoT: initial revision

--- HeadsOrTails.java
+++ HeadsOrTails.java
@@ -1,39 +1,26 @@
 import java.util.Random;
-import java.util.Scanner;
 
-public class HeadsOrTails {
+public class HeadsOrTailsGame {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
-        
-        System.out.print("Who are you?\n> ");
-        String name = scanner.nextLine();
-        System.out.println("Hello, " + name + "!");
-        
         System.out.println("Tossing a coin...");
-        String[] results = new String[3];
-        
-        for (int i = 0; i < 3; i++) {
-            results[i] = random.nextBoolean() ? "Heads" : "Tails";
-            System.out.println("Round " + (i + 1) + ": " + results[i]);
-        }
-        
+
+        // Simulate 3 rounds of coin tossing
         int headsCount = 0;
         int tailsCount = 0;
-        for (String result : results) {
+        Random random = new Random();
+
+        for (int i = 1; i <= 3; i++) {
+            String result = random.nextBoolean() ? "Heads" : "Tails";
+            System.out.println("Round " + i + ": " + result);
+
             if (result.equals("Heads")) {
                 headsCount++;
             } else {
                 tailsCount++;
             }
         }
-        
+
+        // Print total counts
         System.out.println("Heads: " + headsCount + ", Tails: " + tailsCount);
-        
-        if (headsCount > tailsCount) {
-            System.out.println(name + " won!");
-        } else {
-            System.out.println(name + " lost!");
-        }
     }
 }
